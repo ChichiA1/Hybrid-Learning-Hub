b7982d23160b65ced390c29aabaf87734e0ef9e2
ChineloKOA <diamondc1st@gmail.com>
2025-02-24 20:44:11 +0000

    dockerised app and database

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # Hybrid-Learning-Hub
 physical (bricks &amp; mortal) and online digital learning hub
+
+1. alembic revision --autogenerate -m "create_main_tables"
+2. alembic upgrade head - whenever you update the table
+3. alembic downgrade -1 - to rollback 1 migration step
+4. docker-compose exec postgres_db psql -h localhost -U postgres --dbname=postgres
+
+- \l - list all databases
+- \d+ - list all tables (relations) in the current database
+- \dT+ - list all enums in the current database
+- \c postgres - connect to the postgres database
+- \d users - describe the users table and the associated columns
+
+- openssl rand -hex 32
---
+++ backend/app/db/repositories/books.py
@@ -0,0 +1,63 @@
+from sqlalchemy.future import select
+from sqlalchemy.ext.asyncio import AsyncSession
+from app.orm_model.book import Book
+from app.models.books import BookModel, BookStatus
+from sqlalchemy.exc import SQLAlchemyError
+from app.api.dependencies.database import add_update_table
+import logging
+
+# Setting up logging
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+class BookRepository:
+    """"
+    All database actions associated with the Books resource
+    """
+    # The retrieve_book_quantity function to get the quanty by book_id from DB
+    async def retrieve_book_quantity(db: AsyncSession, book_id: int) -> Book:
+        try:
+            result = await db.execute(select(Book).filter(Book.book_id == book_id))
+
+            # result is an instance of sqlalchemy.engine.Result
+            # scalar_one_or_none() will return the first result or None
+            book = result.scalar_one_or_none()
+
+            return book  # Return the quantity of the book
+
+        except (SQLAlchemyError, Exception) as e:
+            # Handle any SQLAlchemy-related or unexpected errors
+            logger.error(f"Error retrieving book quantity in the database: {e}")
+            raise  # Re-raise the exception so the caller knows something went wrong
+
+
+    # The add_book function to add books and commit to the DB
+    async def add_book(db: AsyncSession, book: BookModel):
+        try:
+            # find out if book exist in the database and quantity there is
+            book_in_bd = await BookRepository.retrieve_book_quantity(db,book.book_id)
+            # add current quantity to the added quantity to make the new quantity if book exist in DB
+            if  book_in_bd is None:
+                book_data = book.dict()
+                book_data.update({
+                    'status': BookStatus.available
+                })
+                # Create new Book instance
+                db_book = Book(**book_data)
+                # This stages db_book object to be added to the database, not asynchronous,
+                # local opertation that happens in memory
+                db.add(db_book)
+                db_book = await add_update_table(db, db_book)
+                return db_book.to_dict()
+            else:
+                book_in_bd.quantity += book.quantity
+                book_in_bd = await add_update_table(db, book_in_bd)
+                return book_in_bd.to_dict()
+        except (SQLAlchemyError, Exception) as e:
+            # Handle any SQLAlchemy-related or unexpected errors
+            logger.error(f"Error retrieving book quantity in the database: {e}")
+            raise  # Re-raise the exception so the caller knows something went wrong
+
+
+
+
