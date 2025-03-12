1ed231f3d28049beac4b7eb3139f214b54163fe2
ChineloKOA <diamondc1st@gmail.com>
2025-03-12 13:02:41 +0000

    latest update

--- backend/tests/test_functional.py
+++ backend/tests/test_functional.py
@@ -1,7 +1,3 @@
-def some_example_function(a, b):
-    return a + b
-
-
 # test function
-def test_my_feature_example():
-    assert some_example_function(2, 3) == 5
+def test_addition():
+    assert 1 + 1 == 2
